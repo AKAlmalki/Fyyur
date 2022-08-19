82e4cdc17fcb1f3706d1033b2dba5e43ef1c6b69
AKAlmalki <88806424+AKAlmalki@users.noreply.github.com>
2022-08-19 15:39:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
-# Connect to the database
+# Connect to the database AND setting up the env variables
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'abc')
+DB_NAME = os.getenv('DB_NAME', 'fyyur')
+
+DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 
 # TODO IMPLEMENT DATABASE URL [done]
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:abc@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = DB_PATH
